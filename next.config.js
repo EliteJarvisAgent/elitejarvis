047ed7a36ab3c2c92b929310c11d82a8838bfc27
Jarvis <jarvis@eliteai.io>
2026-04-28 21:14:55 -0400

    Fix: remove static export so API routes work as Vercel serverless functions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',
-  distDir: 'dist',
   images: { unoptimized: true },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
